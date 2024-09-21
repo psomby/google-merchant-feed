adc5246d503587189f8a9b06630ba07e2990ed87
psombekke@intermadix.com <psombekke@intermadix.com>
2024-09-21 10:07:31 +0200

    up

--- src/models/price.ts
+++ src/models/price.ts
@@ -5,6 +5,7 @@ export namespace Price {
   };
 
   export function formatter(price: Price.Model): string {
+    console.log(price)
     return `${price.value.toFixed(2)} ${
       price.currency && price.currency.toUpperCase()
     }`;
